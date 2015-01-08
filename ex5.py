66fed9699cd5f33c356a2294443973ebd57d0792
work machine <DEVRON@DEVRON-MAC.local>
2015-01-08 15:24:53 -0500

    fixed copy

--- ex5.py
+++ ex5.py
@@ -1,17 +1,23 @@
-_name = "Zed A Shaw"
-_age = 35 #not a lie
-_height = 74 #inches
-_weight = 180 #lbs
-_eyes = "Blue"
-_teeth = "White"
-_hair = "Brown"
+import math
 
-print "let's talk about %s" % _name
-print "He's %d inches tall" % _height
-print "He's %d pounds heavy" % _weight
+name = "Zed A Shaw"
+age = 35 #not a lie
+height = 74 #inches
+weight = 180 #lbs
+eyes = "Blue"
+teeth = "White"
+hair = "Brown"
+
+print "let's talk about %s" % name
+print "He's %d inches tall" % height
+print "He's %d pounds heavy" % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair" % (_eyes, _hair)
-print "His teeth are usually %s depending of the coffee." % _teeth
+print "He's got %s eyes and %s hair" % (eyes, hair)
+print "His teeth are usually %s depending of the coffee." % teeth
 
 #this line is tricky, try to get it exactly right 
-print "If I add %d, %d, and %d I get %d" %  (_age, _height, _weight, _age + _height + _weight)
+print "If I add %d, %d, and %d I get %d" %  (age, height, weight, age + height + weight)
+
+print "Convert IN to CM = %r" % float(height * 2.54)
+
+print "Convert LB to KILO = %d" % round(weight * 0.453592)
