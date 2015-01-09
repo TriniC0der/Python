fee7c610a9322da69880142129084354a1a645f7
TriniCoder <OMEGA@Mac-Pro.local>
2015-01-09 08:32:35 -0500

    exe6

--- ex6.py
+++ ex6.py
@@ -1,23 +1,12 @@
-import math
 
-name = "Zed A Shaw"
-age = 35 #not a lie
-height = 74 #inches
-weight = 180 #lbs
-eyes = "Blue"
-teeth = "White"
-hair = "Brown"
+varA = 'x'
+varB = 'x'
 
-print "let's talk about %s" % name
-print "He's %d inches tall" % height
-print "He's %d pounds heavy" % weight
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair" % (eyes, hair)
-print "His teeth are usually %s depending of the coffee." % teeth
-
-#this line is tricky, try to get it exactly right 
-print "If I add %d, %d, and %d I get %d" %  (age, height, weight, age + height + weight)
-
-print "Convert IN to CM = %r" % float(height * 2.54)
-
-print "Convert LB to KILO = %d" % round(weight * 0.453592)
+if isinstance(varA, basestring) or isinstance(varB, basestring):
+    print("string involved")
+elif varA > varB:
+	print("bigger")
+elif varA == varB:
+	print("equal")
+elif varA < varB:
+	print("smaller")
