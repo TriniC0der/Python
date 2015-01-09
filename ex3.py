96fe42945b7e2a42c97a40335cfd36a0b6b2e268
Ronald Foster <ronald.foster@me.com>
2015-01-08 22:28:27 -0500

    new

--- ex3.py
+++ ex3.py
@@ -1,33 +1,12 @@
-print "I will now count my chickens"
-
-print "Hens", 25 + 30 / 6
-
-print "Roosters", 100 - 25 * 3 % 4
-
-print "Now I will count the eggs."
-
-print 3+2+1 -5 +4 %2 -1 /4 +6
-
-print "Is it true that 2+3<5-7?"
-
-print 3 + 2 < 5 - 7
-
-print "What is 3 + 2?", 3 + 2
-
-print "What is 5 - 7?", 5 - 7
-
-print "Oh, that's why it's False."
-
-print "How about some more."
-
-print "Is it greater?", 5 > -2
-
-print "Is it greater or euqal?", 5 >= -2
-
-print "Is it less or equal?", 5 <= -2
 
 
-print 7 / 4
-print 7.0 / 4.0
 
-print "track changes!"
+#discription 
+name = raw_input ("Enter your name: ")
+
+print name
+
+
+y = float(raw_input('enter a number: '))
+
+print y
