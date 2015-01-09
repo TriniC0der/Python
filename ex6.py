7ee7c1abcdc47067674ff70631809472fa62b7c7
work machine <DEVRON@DEVRON-MAC.local>
2015-01-09 15:54:47 -0500

    exe6/7

--- ex6.py
+++ ex6.py
@@ -1,12 +1,20 @@
+x = "There are %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
 
-varA = 'x'
-varB = 'x'
+print x
+print y
 
-if isinstance(varA, basestring) or isinstance(varB, basestring):
-    print("string involved")
-elif varA > varB:
-	print("bigger")
-elif varA == varB:
-	print("equal")
-elif varA < varB:
-	print("smaller")
+print "I said %r." % x
+print "I also said %s." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e 
