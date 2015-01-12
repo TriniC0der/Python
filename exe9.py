b03374185e27ab24eb6b53ab4f10030f30c82f48
work machine <DEVRON@DEVRON-MAC.local>
2015-01-12 15:52:36 -0500

    updated months
    put months in a new line.

--- exe9.py
+++ exe9.py
@@ -1,5 +1,5 @@
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 print "Here are the days: ", days
 print "Here aer the Months: ", months
 print """
