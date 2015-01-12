ff5d8bed7dbe8ebe8103c9c1f05de15c8cf43520
work machine <DEVRON@DEVRON-MAC.local>
2015-01-12 15:54:07 -0500

    minor update to output

--- exe9.py
+++ exe9.py
@@ -2,6 +2,7 @@ days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 print "Here are the days: ", days
 print "Here aer the Months: ", months
-print """
+print 
+"""
 There's something going on here. With the three double-quotes. We'll be able to type as much as we like. Even 4 lines fi we want. or 5 or 6.
 """
